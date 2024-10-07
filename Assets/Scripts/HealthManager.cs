762723579bee7845dc8160ca784a37af047cbe05
SarahGreenfield <sarah.greenfield@csu.fullerton.edu>
2024-10-06 18:21:26 -0700

    Health bar functions better now! :)

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -7,39 +7,39 @@ using UnityEngine.SceneManagement;
 //simple health bar tutorial used: https://www.youtube.com/watch?v=0tDPxNB2JNs
 public class HealthManager : MonoBehaviour
 {
-
+    
     //usable on Unity for the Image 
     [SerializeField] public Image healthBar;
+    public float maxHP;
     
-    public float currentHealth = 100f;  //health at 100% at the start
+    public float currentHealth = 100f; //health at 100% at the start
+
+    void Start(){
+        maxHP = currentHealth; //setting official max health
+    }
 
     // Update is called once per frame
     void Update()
     {
+        healthBar.fillAmount = Mathf.Clamp(currentHealth / maxHP, 0, 1); //to ensure the health doesn't overflow
         //if the player's health is zero (or less than zero for any overkill) The scene reloads using SceneManager and the build setting index for that scene
         if (currentHealth <=0 ){
             SceneManager.LoadSceneAsync(1);
         }
 
         //As of now, the only way to actually take damage is to press the ENTER button, I have it here to show that it is up, just not fully enabled with collisions with other objects.
-       if (Input.GetKeyDown(KeyCode.Return)){
-        Damage(20); //amount of damage taken
-       } 
+       
+        
     }
-
-    // private void hurt(Collider2D collision){
-
-    //     if(collision.tag == "Enemy"){
-    //         Damage(20);
-    //     }
-    // }
 
     
     //The Damage function to reduce the current health with how much percent of their health they have left.
 
     public void Damage(float damage){
-        currentHealth -= damage;
-        healthBar.fillAmount = currentHealth / 100f; 
+        if (currentHealth > 0){
+            currentHealth -= damage;
+            //healthBar.fillAmount = currentHealth / 100f;
+        }
     }
 
 
